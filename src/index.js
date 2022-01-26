e8a03b7811b480e03305b7210061accf4ac59082
soaresmilton <eng.milton.soares@gmail.com>
2022-01-26 15:36:59 -0300

    Added feature that read an item price and return it to console

--- src/index.js
+++ src/index.js
@@ -3,9 +3,35 @@ const $ = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
-async function priceTracking() {
+const url = 'https://www.amazon.com.br/As-obras-revolucion%C3%A1rias-George-Orwell/dp/B08ZK6N3XZ';
 
+
+async function configureBrowser() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+  return page;
 };
 
+async function checkPrice(page) {
+  await page.reload();
+  let html = await page.evaluate(() => document.body.innerHTML);
 
-priceTracking();
+  let itemPrice = await page.evaluate(() => {
+    return document.querySelector('#price').innerText;
+  });
+
+  console.log(itemPrice);
+
+  // $('#price', html).each(function () {
+  //   let price = $(this).text();
+  //   console.log(price);
+  // });
+}
+
+async function monitor() {
+  let page = await configureBrowser();
+  await checkPrice(page);
+}
+
+monitor();
