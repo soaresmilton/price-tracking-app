94cad4400d89625e27b7452344886201fc7cf7c5
soaresmilton <eng.milton.soares@gmail.com>
2022-01-26 13:52:30 -0300

    Index file initial strucutre

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
-
+const $ = require('cheerio');
+const CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
 
 async function priceTracking() {
 
